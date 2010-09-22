bc9a6a9d24de18ed47f8a4614dd158f2ba5b5b9c
William Swanson <swansontec@gmail.com>
2010-09-21 19:36:24 -0700

    Split generate() into several functions

--- source/main.c
+++ source/main.c
@@ -75,76 +75,15 @@ int options_parse(Options *self, int argc, char *argv[])
 }
 
 /**
- * Opens the file given in filename, parses it, processes it, and writes the
- * results to the output file.
- * @return 0 for failure
+ * Performs code-generation into the output file given in the options.
  */
-int generate(FILE *out, String filename, int debug)
+int generate(AstCode *code, Options *opt)
 {
-  Pool pool;
-  Context ctx;
-  AstCode *code;
-
-  CHECK_MEM(pool_init(&pool, 0x10000)); /* 64K block size */
-  ctx.pool = &pool;
-
-  ctx.scope = 0;
-  context_scope_push(&ctx);
-  CHECK(ctx.scope);
-
-  /* Parse the input file: */
-  ctx.filename = filename;
-  ctx.file = string_load(filename);
-  if (!string_size(ctx.file)) {
-    char *s = string_to_c(filename);
-    fprintf(stderr, "error: Could not open source file \"%s\"\n", s);
-    free(s);
-    return 0;
-  }
-  ctx.cursor = ctx.file.p;
-  CHECK(parse_code(&ctx, 0));
-  code = ast_to_code(ctx.out);
-  string_free(ctx.file);
-
-  if (debug) {
-    printf("--- AST: ---\n");
-    dump_code(code, 0);
-  }
-
-  CHECK(generate_code(out, code));
-
-  pool_free(&pool);
-  return 1;
-}
-
-/**
- * Program entry point. Constructs and launches the main program object.
- */
-int main(int argc, char *argv[])
-{
-  int rv;
-  Options opt;
   char *s;
   FILE *file_out;
 
-  options_init(&opt);
-  if (!options_parse(&opt, argc, argv)) {
-    fprintf(stderr, "Usage: %s [-d] [-o output-file] <input-file>\n", argv[0]);
-    return 1;
-  }
-
-  /* Determine output file name: */
-  if (string_size(opt.name_out)) {
-    s = string_to_c(opt.name_out);
-  } else {
-    if (string_rmatch(opt.name_in, string_init_l(".ol", 3)) != 3) {
-      fprintf(stderr, "Error: If no output file is specified, the input file name must end with \".ol\".\n");
-      return 1;
-    }
-    s = string_to_c(string_init(opt.name_in.p, opt.name_in.end - 3));
-  }
-
-  /* Open output file: */
+  s = string_to_c(opt->name_out);
+  CHECK_MEM(s);
   file_out = fopen(s, "wb");
   if (!file_out) {
     fprintf(stderr, "error: Could not open output file \"%s\"\n", s);
@@ -153,8 +92,81 @@ int main(int argc, char *argv[])
   }
   free(s);
 
-  /* Munchify files: */
-  rv = generate(file_out, opt.name_in, opt.debug);
+  CHECK(generate_code(file_out, code));
+
   fclose(file_out);
-  return !rv;
+  return 1;
+}
+
+int main_context_init(Context *ctx, Pool *pool, Options *opt)
+{
+  /* Pool: */
+  CHECK_MEM(pool_init(pool, 0x10000)); /* 64K block size */
+  ctx->pool = pool;
+
+  /* Scope: */
+  ctx->scope = 0;
+  context_scope_push(ctx);
+  CHECK(ctx->scope);
+
+  /* Input stream: */
+  ctx->filename = opt->name_in;
+  ctx->file = string_load(ctx->filename);
+  if (!string_size(ctx->file)) {
+    fprintf(stderr, "error: Could not open source file \"");
+    fwrite(ctx->filename.p, string_size(ctx->filename), 1, stderr);
+    fprintf(stderr, "\"\n");
+    return 0;
+  }
+  ctx->cursor = ctx->file.p;
+
+  return 1;
+}
+
+void main_context_free(Context *ctx)
+{
+  string_free(ctx->file);
+  if(ctx->pool) pool_free(ctx->pool);
+}
+
+/**
+ * Program entry point. Constructs and launches the main program object.
+ */
+int main(int argc, char *argv[])
+{
+  Options opt;
+  Context ctx = {0};
+  Pool pool;
+
+  /* Read the options: */
+  options_init(&opt);
+  if (!options_parse(&opt, argc, argv)) {
+    fprintf(stderr, "Usage: %s [-d] [-o output-file] <input-file>\n", argv[0]);
+    return 1;
+  }
+
+  /* Determine output file name: */
+  if (!string_size(opt.name_out)) {
+    if (string_rmatch(opt.name_in, string_init_l(".ol", 3)) != 3) {
+      fprintf(stderr, "error: If no output file is specified, the input file name must end with \".ol\".\n");
+      return 1;
+    }
+    opt.name_out = string_init(opt.name_in.p, opt.name_in.end - 3);
+  }
+
+  /* Do outline2c stuff: */
+  if (!main_context_init(&ctx, &pool, &opt)) goto error;
+  if (!parse_code(&ctx, 0)) goto error;
+  if (opt.debug) {
+    printf("--- AST: ---\n");
+    dump_code(ast_to_code(ctx.out), 0);
+  }
+  if (!generate(ast_to_code(ctx.out), &opt)) goto error;
+
+  main_context_free(&ctx);
+  return 0;
+
+error:
+  main_context_free(&ctx);
+  return 1;
 }
